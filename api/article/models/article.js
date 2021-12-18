c81fee8e15267b465360a0f5b4ea7bff5b1866f2
Magle-corp <80600877+Magle-corp@users.noreply.github.com>
2021-12-18 18:36:56 +0100

    Collection slug + component internal_link (#12)

--- api/article/models/article.js
+++ api/article/models/article.js
@@ -1,8 +1,46 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async afterUpdate(result, params, data) {
+      // The Article is published.
+      if (result.published_at !== null && data.published_at) {
+        strapi.query("slug").create({
+          title: result.title,
+          slug: result.slug,
+          related_page: `article_${result.id}`,
+        });
+      }
+
+      // The Article is updated.
+      if (result.published_at !== null && !data.published_at) {
+        const related_slug = await strapi
+          .query("slug")
+          .find({ related_page: `article_${result.id}` });
+
+        strapi.query("slug").update(
+          { id: related_slug[0].id },
+          {
+            title: result.title,
+            slug: result.slug,
+            related_page: `article_${result.id}`,
+          }
+        );
+      }
+
+      // The Article isn't published.
+      if (result.published_at == null) {
+        const related_slug = await strapi
+          .query("slug")
+          .find({ related_page: `article_${result.id}` });
+
+        strapi.query("slug").delete({ id: related_slug.id });
+      }
+    },
+  },
+};

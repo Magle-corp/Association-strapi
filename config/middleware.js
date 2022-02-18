40a0d0de0a8b4fe30fc3374720bc30a9b50f1cad
Magle-corp <wolff.kevin@protonmail.com>
2022-02-18 15:08:34 +0100

    Add domain with www for CORS

--- config/middleware.js
+++ config/middleware.js
@@ -3,18 +3,19 @@ module.exports = ({ env }) => ({
     cors: {
       enabled: true,
       origin: [
-        // Local frontend.
+        // Local.
         env("LOCAL_FRONTEND", "http://localhost:3000"),
-        // Local backend.
         env("LOCAL_BACKEND", "http://localhost:1337"),
-        // Staging frontend.
+        // Staging.
         env("STAGING_FRONTEND", "http://localhost"),
-        // Staging backend.
+        env("STAGING_FRONTEND_WWW", "http://localhost"),
         env("STAGING_BACKEND", "http://localhost"),
-        // Production frontend.
+        env("STAGING_BACKEND_WWW", "http://localhost"),
+        // Production.
         env("PROD_FRONTEND", "http://localhost"),
-        // Production backend.
+        env("PROD_FRONTEND_WWW", "http://localhost"),
         env("PROD_BACKEND", "http://localhost"),
+        env("PROD_BACKEND_WWW", "http://localhost"),
       ],
     },
   },

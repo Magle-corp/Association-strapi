bf7d0645871c0497c2edfa8e7de1ecbb23f69743
Kévin Wolff <80600877+Magle-corp@users.noreply.github.com>
2022-02-18 11:09:22 +0100

    Collection message (#16)

--- config/middleware.js
+++ config/middleware.js
@@ -3,18 +3,18 @@ module.exports = {
     cors: {
       enabled: true,
       origin: [
-        // Local frontend, needed for fetch.
-        "http://localhost",
-        // Local backend, needed for access admin.
+        // Local frontend.
+        "http://localhost:3000",
+        // Local backend.
         "http://localhost:1337",
-        // Staging frontend, needed for fetch.
+        // Staging frontend.
         "http://association-next-staging.magle.fr",
-        // Staging backend, needed for access admin.
+        // Staging backend.
         "http://association-strapi-staging.magle.fr",
-        // Production frontend, needed for fetch.
+        // Production frontend.
         "http://association-next.magle.fr",
-        // Production backend, needed for access admin.
-        "http://association-next.magle.fr",
+        // Production backend.
+        "http://association-strapi.magle.fr",
       ],
     },
   },
